a0d1ffc437680f0b0ee7aeaf34c5492dc5dfe36a
Tech Rules <66055840+techrules07@users.noreply.github.com>
2025-12-31 18:46:54 +0530

    otp error message

--- src/main/java/com/smartstay/tenant/service/UserService.java
+++ src/main/java/com/smartstay/tenant/service/UserService.java
@@ -100,7 +100,7 @@ public class UserService {
 
 
         } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("You are not belongs to any hostels.", HttpStatus.BAD_REQUEST);
         }
 
     }
