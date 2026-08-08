1f00a2cff01e032d61ebfb83af83bd9d13fb3aa9
ArunS3Remotica <arun.t@s3remotica.com>
2026-08-08 13:01:41 +0530

    cancelled status not showing fix

--- src/main/java/com/smartstay/tenant/service/CustomerService.java
+++ src/main/java/com/smartstay/tenant/service/CustomerService.java
@@ -21,6 +21,7 @@ import com.smartstay.tenant.response.customer.EditCustomer;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -64,6 +65,7 @@ public class CustomerService {
     @Autowired
     private JWTService jwtService;
     @Autowired
+    @Lazy
     private BedsService bedsService;
 
     public ResponseEntity<?> getCustomerDetails() {
